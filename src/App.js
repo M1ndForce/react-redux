6beb953dfb4ae4d98637769d3cfae61d998b80c4
M1ndForce <66359796+M1ndForce@users.noreply.github.com>
2020-11-05 23:28:13 +0200

    m

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container pt-3">
       <h1>React redux</h1>
+      <div className="row">
+        <div className="col">FORM</div>
+      </div>
+      <div className="row">
+        <div className="col">FETCHED POSTS</div>
+      </div>
     </div>
   );
 }
